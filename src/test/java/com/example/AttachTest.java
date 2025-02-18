39b7b2ac58fbb31448d873cfee2de317914e4aae
Manju R <cskmnrpt@gmail.com>
2025-02-18 18:35:41 +0530

    fix: pom.xml

--- src/test/java/com/example/AttachTest.java
+++ src/test/java/com/example/AttachTest.java
@@ -8,7 +8,7 @@ public class AttachTest {
 
     @Test
     public void testWithFileAttachment() {
-        Qase.attach("./src/test/java/com/example/attachments/test-file.txt");
+        Qase.attach("src/test/java/com/example/attachments/test-file.txt");
         System.out.println("passed");
     }
 
